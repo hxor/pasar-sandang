9841fa4442ad063aa8b8b49ca879dadd8a49733c
hxor <hakimsembiring@outlook.co.id>
2018-02-19 22:49:38 +0700

    add assets and package

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.front.app');
+});
+
+Route::get('/admin', function () {
+    return view('layouts.back.app');
 });
 
 Auth::routes();
